a616331a46bdcc01de2b6f9457088b84b22a28dd
jfen445 <79815130+jfen445@users.noreply.github.com>
2021-09-20 22:12:00 +1200

    Arranged components on the home page

--- src/containers/pages/HomePage.jsx
+++ src/containers/pages/HomePage.jsx
@@ -1,10 +1,52 @@
 import React from "react";
+import { Grid } from "@material-ui/core";
 import Weather from "../../components/WeatherComponent/Weather";
+import Spotify, { accessUrl } from "../../components/SpotifyComponent/Spotify";
+import Time from "../../components/CurrentTimeComponent/Time";
+import Reminder from "../../components/RemindersComponent/Reminder";
+import styles from "../../styling/HomePage.module.scss";
 
 export default function HomePage() {
   return (
     <>
-      <Weather />
+      <Grid
+        // going accross
+        container
+        direction="column"
+        justifyContent="full"
+        alignItems="stretch"
+        className={styles.full}
+      >
+        <Grid style={{ background: "black" }}>hello</Grid>
+        <Grid
+          // going accross
+          container
+          direction="columns"
+          justifyContent="full"
+          alignItems="stretch"
+          className={styles.full}
+        >
+          <Grid style={{ background: "black" }}>Hello</Grid>
+          <Grid style={{ background: "grey" }}>
+            <div className={styles.myDashboard}>My Dashboard</div>
+            <div className={styles.time}>
+              <Time />
+            </div>
+            <div className={styles.blocks}>
+              <Weather />
+            </div>
+            <div className={styles.blocks}>
+              <Spotify />
+            </div>
+            <a href={accessUrl}>LOGIN TO SPOTIFY</a>
+          </Grid>
+
+          <Grid style={{ background: "black" }}>Hello</Grid>
+        </Grid>
+        <Grid style={{ background: "black" }}>hello</Grid>
+      </Grid>
+
+      {/* <Reminder /> */}
     </>
   );
 }
