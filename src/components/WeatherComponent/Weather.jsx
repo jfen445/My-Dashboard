54bc41e36e510dd6ab79b4a798b4d3572931d0ba
jfen445 <79815130+jfen445@users.noreply.github.com>
2021-09-20 23:03:28 +1200

    Cleaned up warnings

--- src/components/WeatherComponent/Weather.jsx
+++ src/components/WeatherComponent/Weather.jsx
@@ -1,13 +1,10 @@
 import React, { useState, useEffect } from "react";
 // eslint-disable-next-line import/no-unresolved
-import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
-import Paper from "@material-ui/core/Paper";
 import ButtonBase from "@material-ui/core/ButtonBase";
 import { useGet } from "../../utils/crudHooks";
 import styles from "../../styling/WeatherComponent.module.scss";
-import cloudimg from "./images/cloudrain.png";
 
 function Weather() {
   const [weatherInfo, setWeatherInfo] = useState(null);
