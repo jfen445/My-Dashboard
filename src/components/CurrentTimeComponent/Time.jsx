25bd4e8278093b5ad38c9711cf6128f132f1d4d3
jfen445 <79815130+jfen445@users.noreply.github.com>
2021-09-20 22:10:52 +1200

    Implemented component to get the current time

---
+++ src/components/CurrentTimeComponent/Time.jsx
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from "react";
+
+function Time() {
+  const [time, setTime] = useState(null);
+  useEffect(() => {
+    // setTime(Date().toLocaleString());
+    const current = new Date();
+    // By default US English uses 12hr time with AM/PM
+    const timee = current.toLocaleTimeString("en-US", {
+      hour: "2-digit",
+      minute: "2-digit",
+    });
+
+    console.log(time);
+    setTime(timee);
+  });
+
+  return <div>{time}</div>;
+}
+
+export default Time;
